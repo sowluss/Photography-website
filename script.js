ace620a98b7a0e9803adc6f77222dd128256204b
sowluss <sowluss@gmail.com>
2018-05-04 01:40:27 +0300

    added modified script.js file

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 //When the user clicks on the button toggle between hiding and showing the dropdown content 
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 //Close the dropdown if the user clicks outside of it
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-  
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
 
-// jQuery
 
-// right button disabled
+// Right button disabled to prevent from dowloading images
 
-$("img").bind('contextmenu', function(e){
-  return false;
-});
+function nocontext(e) {
+  var clickedTag = (e==null) ? event.srcElement.tagName : e.target.tagName;
+  if (clickedTag == "IMG")
+    return false;
+}
+document.oncontextmenu = nocontext;
 
- 
+
